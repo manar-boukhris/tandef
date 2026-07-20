39eeacef3a7aac82a8170148e956d076b4603577
manar-boukhris <manar.boukhris@esprit.tn>
2026-07-20 21:22:37 +0200

    Fix JSX namespace type error1

--- components/ServiceLandingPage.tsx
+++ components/ServiceLandingPage.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 
-import { useState, type ReactNode } from 'react';
+import { useState, useEffect, type ReactNode } from 'react';
 import { ServiceHeroArt } from './ServiceHeroArt';
 import type { ServiceLanding } from '@/lib/serviceLandingData';
 
