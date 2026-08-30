ac8cda3508c2534b3e1d2dabd74d8af458074e84
manar-boukhris <manar.boukhris@esprit.tn>
2026-08-30 18:41:57 +0200

    app config finalee version 5

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -32,27 +32,16 @@ export default function ImpressumPage() {
         Handelsregister: Amtsgericht Köln<br />
         Registernummer: HRB 12944 
       </p>
-      <p style={{fontSize: '.85rem', fontStyle: 'italic'}}>
-        (Falls die Gesellschaft noch nicht im Handelsregister eingetragen ist, ist dieser Abschnitt bis zur Eintragung zu entfernen.)
-      </p>
+      
 
       <h2>Umsatzsteuer-Identifikationsnummer</h2>
       <p>
         Umsatzsteuer - Identifikationsnummer gemäß § 27a Umsatzsteuergesetz:<br />
         DE450493694
       </p>
-      <p style={{fontSize: '.85rem', fontStyle: 'italic'}}>
-        (Nur angeben, sofern bereits erteilt.)
-      </p>
+      
 
-      <h2>Verantwortlich für den Inhalt</h2>
-      <p>
-        Verantwortlich für journalistisch-redaktionelle Inhalte (soweit vorhanden):<br />
-        Yousef Alshwakh<br />
-        Keupstrasse 123 <br />
-        51063 Köln<br />
-        Deutschland<br />
-      </p>
+      
 
       <h2>Verbraucherstreitbeilegung</h2>
       <p>
